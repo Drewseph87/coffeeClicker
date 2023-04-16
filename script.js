a4c9c00834693d9cc1e8087c232b3fbdeec8c21d
Drew Harris <drewseph87@gmail.com>
2023-04-16 13:28:29 -0600

    Updated most files to be where I need it to be, checking to see if liveServer error of coffee is not clicking due to code or real error

--- script.js
+++ script.js
@@ -2,11 +2,10 @@
 const data = window.data;
 
 // Make your References to the two DOM nodes
-const 
 // Create a reference to the element who's ID is 'big_coffee and call it bigCoffee
-
+const bigCoffee = document.getElementById('big_cofffee');
 // Create a reference to the element who's ID is 'producer_container' and call it producerContainer
-
+const producerContainer = document.getElementById('producer_container');
 /**************
  *   SLICE 1
  **************/
@@ -14,10 +13,10 @@ const
 function updateCoffeeView(coffeeQty) {
 
   // Create a reference to the element who's ID is 'coffee_counter'
-  const coffeeCounter = document.getElementById('coffee_counter')
+  const coffeeCounter = document.getElementById('coffee_counter');
 
   // Set the innerText of that element to be the coffeeQty passed into this function
-  coffeeCounter.innerText = coffeeQty
+  coffeeCounter.innerText = coffeeQty;
 }
 
 function clickCoffee(data) {
@@ -34,12 +33,12 @@ function clickCoffee(data) {
  **************/
 
 function unlockProducers(producers, coffeeCount) {
+  // loop through the producers array passed into the function
   for(i = 0; i < producers.length; i++) {
-    // loop through the producers array passed into the function
-    if(coffeeCount >= producer.price/2){
-      // for each producer, if the coffeeCount (passed in) is greater than or equal
+    // for each producer, if the coffeeCount (passed in) is greater than or equal
+    if(coffeeCount >= producers[i].price /2){ // go into values to find the objects and divide by two. Parenthesis to make sure this occurs first like math before comparison to coffeeCount
       // to half the producer's price, reassign the producers.unlocked property to equal true
-      producers.unlocked = true;
+      producers[i].unlocked = true;
     }
   }
 }
@@ -48,8 +47,12 @@ function getUnlockedProducers(data) {
   // use the Array.prototype.filter() method
   // filter through the data.producers property, and return an array with only the producers whose
   // unlocked property is true
-
+const unlocky = (producerObj) => {
+  return producerObj.unlocked === true;
 }
+return data.producers.filter(unlocky);
+}
+// (producerObj) => {return producerObj.unlocked} -- Shorthanded version to creating a function inside .filter field similar to what's above. Wrote both out to understand...
 
 // You do not need to edit this function
 function makeDisplayNameFromId(id) {
@@ -89,11 +92,12 @@ function deleteAllChildNodes(parent) {
 
 function renderProducers(data) {
   // call the unlockProducers function and pass it data.producers and data.coffee
+  unlockProducers(data.producers, data.coffee);
 
   // make a reference to the DOM element whose ID is producer_container
-
+const producerContainer = document.getElementById('producer_container');
   // call the deleteAllChildNodes function and pass it the above producerContainer element
-
+deleteAllChildNodes(producerContainer);
   // you do not need to edit the following code, but for understanding, this gets the unlocked producers,
   // and for each producer makes a little html div with that producer's info
   getUnlockedProducers(data).forEach((producer) => {
@@ -158,20 +162,26 @@ function buyButtonClick(event, data) {
 function tick(data) {
   // increment the data object's (passed into this function)
   // coffee property by the data.totalCPS amount
-
+data.coffee += data.totalCPS;
   // call the updateCoffeeView function and pass it the data.coffee property
-
+updateCoffeeView(data.coffee);
   // call the renderProducers function and pass it the newly updated data object
-  renderProducers(data);
+renderProducers(data);
 }
 
 // Event Listeners
-
 // add a 'click' event listener to the bigCoffee element (that you referenced above)
 // the event listener should call the clickCoffee function, and pass in the global data object
+bigCoffee.addEventListener('click', function(){
+  clickCoffee(data);
+});
+
 
 // add a 'click' event listener to the element (referenced at the top of the file)
 // the event listener should call the buyButtonClick function and pass it the event, and the global data object
-
+producerContainer.addEventListener('click', (event) => { 
+  const clickTarget = event.target;
+  if (clickTarget.tagName === "BUTTON") { 
+  buyButtonClick(event, data)});
 // You do not need to edit this last line. This simple runs your tick function every 1000ms, or 1s
 setInterval(() => tick(data), 1000);
